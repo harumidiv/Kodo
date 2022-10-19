9ed6f7d73a433115f513ef5fe54a40fde0a729dd
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-19 23:00:29 +0900

    鼓動のプロジェクトを新規作成しました

---
+++ Kodo/KodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodoApp.swift
+//  Kodo
+//
+//  Created by 佐川 晴海 on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct KodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
