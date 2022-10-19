0767352565851ab62a773b56c3543de14179d728
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-19 23:10:36 +0900

    テキストのチラツキを追加しました

--- Kodo/KodoListView.swift
+++ Kodo/KodoListView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct KodoListView: View {
+    @State var timer :Timer?
+    @State var isTapped: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Button(action: {
+            isTapped = true
+            //人間
+            timer = Timer.scheduledTimer(withTimeInterval: 0.85, repeats: true) { _ in
+                Task {
+                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+                    impactHeavy.prepare()
+                    impactHeavy.impactOccurred()
+                    try await Task.sleep(nanoseconds: 400_000_000)
+                    let impactSoft = UIImpactFeedbackGenerator(style: .soft)
+                    impactSoft.prepare()
+                    impactSoft.impactOccurred()
+                }
+            }
+
+        }, label: {
+            if isTapped {
+                Text("人間の心臓")
+                    .padding()
+                    .blinkEffect(interval: 0.425)
+            } else {
+                Text("人間の心臓")
+                    .padding()
+
+            }
+        })
     }
 }
 
