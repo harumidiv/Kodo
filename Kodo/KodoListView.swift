20fd7189762afbc4792e831b5fcf1a47a32a1f98
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-20 10:50:56 +0900

    パラパラアニメーションが動くようにしました

--- Kodo/KodoListView.swift
+++ Kodo/KodoListView.swift
@@ -6,39 +6,21 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct KodoListView: View {
     @State var timer :Timer?
     @State var isTapped: Bool = false
 
     var body: some View {
-        Color.black
-        Button(action: {
-            isTapped = true
-            //人間
-            timer = Timer.scheduledTimer(withTimeInterval: 0.85, repeats: true) { _ in
-                Task {
-                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-                    impactHeavy.prepare()
-                    impactHeavy.impactOccurred()
-                    try await Task.sleep(nanoseconds: 400_000_000)
-                    let impactSoft = UIImpactFeedbackGenerator(style: .soft)
-                    impactSoft.prepare()
-                    impactSoft.impactOccurred()
-                }
-            }
-
-        }, label: {
-            if isTapped {
-                Text("人間の心臓")
-                    .padding()
-                    .blinkEffect(interval: 0.425)
-            } else {
-                Text("人間の心臓")
-                    .padding()
-
-            }
-        })
+        ZStack {
+            Color.black
+            ParaparaAnimationView(duration: 0.85, images: [UIImage(named: "human_1")!,
+                                                           UIImage(named: "human_2")!,
+                                                           UIImage(named: "human_3")!,
+                                                           UIImage(named: "human_4")!])
+            .frame(width: 200, height: 200)
+        }
     }
 }
 
@@ -47,3 +29,33 @@ struct KodoListView_Previews: PreviewProvider {
         KodoListView()
     }
 }
+
+
+
+
+//            Button(action: {
+//                isTapped = true
+//                //人間
+//                timer = Timer.scheduledTimer(withTimeInterval: 0.85, repeats: true) { _ in
+//                    Task {
+//                        let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+//                        impactHeavy.prepare()
+//                        impactHeavy.impactOccurred()
+//                        try await Task.sleep(nanoseconds: 400_000_000)
+//                        let impactSoft = UIImpactFeedbackGenerator(style: .soft)
+//                        impactSoft.prepare()
+//                        impactSoft.impactOccurred()
+//                    }
+//                }
+//
+//            }, label: {
+//                if isTapped {
+//                    Text("人間の心臓")
+//                        .padding()
+//                        .blinkEffect(interval: 0.425)
+//                } else {
+//                    Text("人間の心臓")
+//                        .padding()
+//
+//                }
+//            })
