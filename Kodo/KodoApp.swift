64488345d714299bfc9d682dbc2b8399c0dffa53
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-20 15:59:37 +0900

    テキスト表示に切り替えられるようにしました

--- Kodo/KodoApp.swift
+++ Kodo/KodoApp.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 @main
 struct KodoApp: App {
+    @State private var isText: Bool = false
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                KodoListView()
-            }
-            .toolbar {
-                Button(action: {
-                    // TODO Action
-                }) {
-                    Image(systemName: "textformat.abc.dottedunderline")
+                KodoListView(isText: $isText)
+                    .toolbar
+                {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            isText.toggle()
+                        }) {
+                            Image(systemName: isText ? "photo" : "textformat.abc.dottedunderline")
+                        }
+                        .padding()
+                    }
                 }
             }
         }
--- Kodo/KodoListView.swift
+++ Kodo/KodoListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import UIKit
 
 struct KodoListView: View {
+    @Binding var isText: Bool
+
     struct Creature: Identifiable {
         let id: UUID = UUID()
         let name: String
@@ -78,31 +80,39 @@ struct KodoListView: View {
                 VStack {
                     LazyVGrid(columns: [GridItem(),GridItem(), GridItem()]) {
                         ForEach(creatures) { creature in
-                            ParaparaAnimationView(duration: creature.heartbeat, images: creature.images)
-                                .frame(width: length, height: length)
-                                .onTapGesture{
-                                    if isTapped {
-                                        isTapped = false
-                                        timer?.invalidate()
-                                        timer = nil
-                                    } else {
-                                        isTapped = true
-                                        timer = Timer.scheduledTimer(withTimeInterval: creature.heartbeat, repeats: true) { _ in
-                                            Task {
-                                                let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-                                                impactHeavy.prepare()
-                                                impactHeavy.impactOccurred()
-                                                try await Task.sleep(seconds: creature.heartbeat / 2)
-                                                let impactSoft = UIImpactFeedbackGenerator(style: .soft)
-                                                impactSoft.prepare()
-                                                impactSoft.impactOccurred()
+                            if isText {
+                                Text(creature.name)
+                                    .foregroundColor(.red)
+                                    .frame(width: length, height: length)
+                                    .blinkEffect(interval: creature.heartbeat / 2)
+                                Spacer()
+                            } else {
+                                ParaparaAnimationView(duration: creature.heartbeat, images: creature.images)
+                                    .frame(width: length, height: length)
+                                    .onTapGesture{
+                                        if isTapped {
+                                            isTapped = false
+                                            timer?.invalidate()
+                                            timer = nil
+                                        } else {
+                                            isTapped = true
+                                            timer = Timer.scheduledTimer(withTimeInterval: creature.heartbeat, repeats: true) { _ in
+                                                Task {
+                                                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+                                                    impactHeavy.prepare()
+                                                    impactHeavy.impactOccurred()
+                                                    try await Task.sleep(seconds: creature.heartbeat / 2)
+                                                    let impactSoft = UIImpactFeedbackGenerator(style: .soft)
+                                                    impactSoft.prepare()
+                                                    impactSoft.impactOccurred()
+                                                }
                                             }
                                         }
                                     }
-                                }
+                                Spacer()
+                            }
                         }
                     }
-                    Spacer()
                 }
             }
         }
@@ -111,36 +121,6 @@ struct KodoListView: View {
 
 struct KodoListView_Previews: PreviewProvider {
     static var previews: some View {
-        KodoListView()
+        KodoListView(isText: .constant(true))
     }
 }
-
-
-
-
-//            Button(action: {
-//                isTapped = true
-//                //人間
-//                timer = Timer.scheduledTimer(withTimeInterval: 0.85, repeats: true) { _ in
-//                    Task {
-//                        let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-//                        impactHeavy.prepare()
-//                        impactHeavy.impactOccurred()
-//                        try await Task.sleep(nanoseconds: 400_000_000)
-//                        let impactSoft = UIImpactFeedbackGenerator(style: .soft)
-//                        impactSoft.prepare()
-//                        impactSoft.impactOccurred()
-//                    }
-//                }
-//
-//            }, label: {
-//                if isTapped {
-//                    Text("人間の心臓")
-//                        .padding()
-//                        .blinkEffect(interval: 0.425)
-//                } else {
-//                    Text("人間の心臓")
-//                        .padding()
-//
-//                }
-//            })
