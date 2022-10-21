1f34808317e766c9b1cfd8332c99e2179eea5d81
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-21 18:25:57 +0900

    スライドをボタンタップで行えるようにしました

--- Kodo/ImageSliderView.swift
+++ Kodo/ImageSliderView.swift
@@ -12,38 +12,78 @@ struct ImageSliderView: View {
 
     @State private var index: Int = 0
     @State private var offset: CGFloat = 0
+    
 
     var body: some View {
         GeometryReader { geometry in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 0) {
-                    ForEach(0..<self.imageNames.count) {
-                        Image(self.imageNames[$0])
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: length, height: length)
+            ZStack {
+                let length = min(geometry.size.width, geometry.size.height)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        ForEach(0..<self.imageNames.count) {
+                            Image(self.imageNames[$0])
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: length, height: length)
+                        }
                     }
+                }
+                .content.offset(x: self.offset)
+                .gesture(DragGesture()
+                    .onChanged{ value in
+                        self.offset = value.translation.width - length * CGFloat(self.index)
+                    }
+                    .onEnded{ value in
+                        let scrollThreshold = length / 2
+                        if value.predictedEndTranslation.width < -scrollThreshold {
+                            self.index = min(self.index + 1, self.imageNames.endIndex - 1)
+                        } else if value.predictedEndTranslation.width > scrollThreshold {
+                            self.index = max(self.index - 1, 0)
+                        }
+
+                        withAnimation {
+                            self.offset = -length * CGFloat(self.index)
+                        }
+                    }
+                )
+                HStack(spacing: 0) {
+                    Button(action: {
+                        withAnimation{
+                            index -= 1
+                            self.offset = -length * CGFloat(self.index)
+                        }
+                    }) {
+                        Image(systemName:"chevron.left")
+                            .font(Font.system(size: ConstantValue.fontSize, weight: .regular))
+                            .frame(width: ConstantValue.buttonWidth, height: ConstantValue.buttonWidth)
+                    }
+                    .disabled(index == 0)
+
+                    Rectangle()
+                        .frame(width: length - ConstantValue.buttonWidth * 2)
+                        .foregroundColor(.clear)
+
+                    Button(action: {
+                        withAnimation{
+                            index += 1
+                            self.offset = -length * CGFloat(self.index)
+                        }
+                    }) {
+                        Image(systemName:"chevron.right")
+                            .font(Font.system(size: ConstantValue.fontSize, weight: .regular))
+                            .frame(width: ConstantValue.buttonWidth, height: ConstantValue.buttonWidth)
+                    }
+                    .disabled(index == imageNames.count)
+
+                    Spacer() // ScrollViewがいて横幅がでかいのでSpacerで左寄せにする
                 }
             }
-            .content.offset(x: self.offset)
-            .gesture(DragGesture()
-                .onChanged{ value in
-                    self.offset = value.translation.width - length * CGFloat(self.index)
-                }
-                .onEnded{ value in
-                    let scrollThreshold = length / 2
-                    if value.predictedEndTranslation.width < -scrollThreshold {
-                        self.index = min(self.index + 1, self.imageNames.endIndex - 1)
-                    } else if value.predictedEndTranslation.width > scrollThreshold {
-                        self.index = max(self.index - 1, 0)
-                    }
-
-                    withAnimation {
-                        self.offset = -length * CGFloat(self.index)
-                    }
-                }
-            )
         }
+    }
+
+    private struct ConstantValue {
+        static let buttonWidth: CGFloat = 32
+        static let fontSize: CGFloat = 28
     }
 }
 
